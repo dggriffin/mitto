28c329fb7e943592e516778e7cee7174323f3ef5
10.89.198.240 <ggriffin@vistaprint.com>
2016-01-15 16:53:22 -0500

    Initial redesign for mitto 0.3.0

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import path from "path";
 import fs from "fs";
 
 const MITTO_CONFIG = '.mitto';
+const DATA_TYPES = ["undefined", "object", "boolean", "number", "string", "symbol", "function"];
 
 /**
  * Find JSON configuration given a filename
  *
  * @return {String} -- the filepath used fort require/import
  */
-export function findConfig(filename) {
+export function loadConfig(filename) {
+  //Does our consumer have a valid .mitto?
+  let mittoObject = _loadMitto();
+  let configObject = _findFile(filename);
+
+  return _validateConfig(configObject, mittoObject);
+};
+
+let _findFile = (filename) => {
     let cwd = process.cwd();
     let parts = cwd.split(path.sep);
     do {
@@ -17,56 +26,107 @@ export function findConfig(filename) {
 
       let file = path.join(loc, filename);
       if (fs.existsSync(file)) {
-        return file;
+        let fileObj = require(file);
+        if(typeof fileObj !== "object") {
+          fileObj = _loadJSON(fileObj);
+        }
+        return fileObj;
       }
 
       parts.pop();
     } while (parts.length);
 
-    return "";
+    return null;
 };
 
-/**
- * Find JSON configuration given .mitto configuration
- *
- * @return {String} -- the filepath used for require/import
- */
-
-export function findMittoConfig() {
-  let mittoPath = findConfig(MITTO_CONFIG);
-  let mittoObj = loadJSON(mittoPath);
-  let packageObj = require(findConfig('package.json'));
-
-  let configPath = findConfig(mittoObj.name);
-  if (!configPath && Object.keys(mittoObj.required).length) {
-    throw new Error(`${mittoObj.name} configuration file not found, and is required by ${packageObj.name}`);
-  }
-  else if (!configPath) {
-    return "";
-  }
-
-  let configObj = require(configPath);
-
-  for (let key in mittoObj.required) {
-    if (!configObj.hasOwnProperty(key)) {
-      throw new Error(`Required property ${key} not found in ${mittoObj.name}`);
-    }
-    else if (typeof configObj[key] !== typeof mittoObj.required[key]) {
-      throw new Error(`Required property ${key} expected to be of type ${typeof configObj[key]}`);
-    }
-  }
-
-  for (let key in mittoObj.optional) {
-    if (typeof configObj[key] !== typeof mittoObj.optional[key]) {
-      throw new Error(`Optional property ${key} expected to be of type ${typeof configObj[key]}`);
-    }
-  }
-
-  return configPath;
-
+let _loadMitto = () => {
+  let mittoObject = _findFile(MITTO_CONFIG);
+  return mittoOjbect ? _validateMitto(mittoObject) : null;
 };
 
-let loadJSON = (file) => {
-  let data = fs.readFileSync(file);
+let _validateMitto = (mittoObject) => {
+
+  if (!mittoObject.hasOwnProperty("name")) {
+     throw new Error(`"name" property is missing from .mitto and is required.`);
+  }
+
+  if (mittoObject.hasOwnProperty("required")) {
+    for (let key in mittoObject.required) {
+      if (key.hasOwnProperty("type")) {
+        if (DATA_TYPES.indexOf(key.type) === -1) {
+          throw new Error(`${key.type} is not a valid data type. Expected: "undefined", "object", "boolean", "number", "string", "symbol", or "function"`);
+        }
+      }
+      else {
+        throw new Error(`"type" property is missing from .mitto's required parameter ${key}`);
+      }
+
+      if (key.hasOwnProperty("description") && typeof key.description !== "string") {
+        throw new Error(`"description" property of .mitto's required parameter ${key} must be of type "string"`);
+      }
+    }
+  }
+
+  if (mittoObject.hasOwnProperty("optional")) {
+    for (let key in mittoObject.optional) {
+      if (key.hasOwnProperty("type")) {
+        if (DATA_TYPES.indexOf(key.type) === -1) {
+          throw new Error(`${key.type} is not a valid data type. Expected: "undefined", "object", "boolean", "number", "string", "symbol", or "function"`);
+        }
+      }
+      else {
+        throw new Error(`"type" property is missing from .mitto's optional parameter ${key}`);
+      }
+
+      if (key.hasOwnProperty("description") && typeof key.description !== "string") {
+        throw new Error(`"description" property of .mitto's optional parameter ${key} must be of type "string"`);
+      }
+
+      if (key.hasOwnProperty("default") && typeof key.default !== key.type) {
+        throw new Error(`"default" property of .mitto's optional parameter ${key} must be a ${key.type}, as specified by "type"`);
+      }
+    }
+  }
+
+  return mittoObject;
+};
+
+let _validateConfig = (configObject, mittoObject) => {
+  let packageObject = require(_findFile('package.json'));
+  if(!mittoObject){
+    return configObject;
+  }
+
+  if (!configObject && Object.keys(mittoObject.required).length) {
+    throw new Error(`${mittoObject.name} configuration file not found, and is required by ${packageObject.name}`);
+  }
+  else if (!configObject) {
+    return configObject;
+  }
+
+  for (let key in mittoObject.required) {
+    if (!configObject.hasOwnProperty(key)) {
+      throw new Error(`Required property ${key} not found in ${mittoObject.name}`);
+    }
+    else if (typeof configObject[key] !== mittoObject.required[key].type) {
+      throw new Error(`Required property ${key} expected to be of type ${mittoObject.required[key].type}`);
+    }
+  }
+
+  for (let key in mittoObject.optional) {
+    if (typeof configObject[key] !== mittoObject.optional[key].type) {
+      throw new Error(`Optional property ${key} expected to be of type ${mittoObject.required[key].type}`);
+    }
+  }
+
+  return configObject;
+};
+
+let _loadJSON = (file) => {
+  try {
+    let data = fs.readFileSync(file);
+  } catch (e) {
+    throw new Error(`${file} expected to be in JSON format`);
+  }
   return JSON.parse(data);
-}
+};
--- test/index.js
+++ test/index.js
@@ -1,22 +1,11 @@
 var should = require('chai').should(),
     mitto = require('../lib/index'),
-    findConfig = mitto.findConfig,
-    findMittoConfig = mitto.findMittoConfig,
     path = require("path");
 
 var filename = 'package.json';
 
-describe('#find', function() {
+describe('#loadConfig', function() {
   it('finds configs that exist', function() {
-    findConfig(filename).should.equal(process.cwd() + path.sep + filename);
+    mitto.loadConfig(filename).should.not.equal(null);
   });
-
-    it('returns empty strings for configs that do not exist', function() {
-    findConfig('packagess.json').should.equal('');
-  });
-
-  it('finds config when it matches .mitto template', function() {
-    findMittoConfig(filename).should.equal(process.cwd() + path.sep + filename);
-  });
-
 });
